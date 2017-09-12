ad6277e146aa730987e3d5fca687bb4298eb8ac9
mo <mo@mombp.local>
2017-09-12 20:52:49 +0800

    17-1

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'video-list',
   templateUrl: './video-list.component.html',
-  styleUrls: ['./video-list.component.css']
+  styleUrls: ['./video-list.component.css'],
+  inputs: ['videos']
 })
 export class VideoListComponent implements OnInit {
 
---
+++ src/app/video.ts
@@ -0,0 +1,8 @@
+// ng g cl Video
+// 對應到mogodb的schema
+export class Video {
+  _id: string;
+  title: string;
+  url: string;
+  description: string;
+}
